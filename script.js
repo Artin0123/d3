4d05ece8b25212f85ca43cd3475f2951804b75ad
Artin Lin <artinlintw@gmail.com>
2022-04-27 09:53:14 +0000

    3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const svg = d3.select('body').append('svg').attr('width', 350).attr('height', 30
 const height_male = svg.append('g').attr('transform', 'translate(100,100)');
 height_male.append('line').attr('x1', 0).attr('x2', 173.5).style('stroke', 'black');
 height_male.append('circle').attr('cx', 173.5).attr('cy', 0).attr('r', 3);
-height_male.append('text').attr('x', 0).attr('y', 20).text("台灣男23生平均身高173.5 cm");
+height_male.append('text').attr('x', 0).attr('y', 20).text("台灣男生平均身高173.5 cm");
 const height_female = svg.append('g').attr('transform', 'translate(100,200)');
 height_female.append('line').attr('x1', 0).attr('x2', 161.5).style('stroke', 'black');
 height_female.append('circle').attr('cx', 161.5).attr('cy', 0).attr('r', 3);
